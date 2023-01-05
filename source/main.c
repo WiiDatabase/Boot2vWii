1c7d3e261a1f0bb03ffe2b7af1bc4298d8ccdaeb
Killerpommes <killerpommes88@web.de>
2023-01-05 19:13:07 +0100

    forcing resolution
    First commit for forcing resolution

--- source/main.c
+++ source/main.c
@@ -16,11 +16,49 @@
 #define DISPLAY BOTH
 #endif
 
+#ifndef FORCERES
+#define FORCERES NONE
+#endif
+
 enum SCREEN_TYPE {
     TV = 1,
     DRC = 2,
     BOTH = 3
 };
+
+enum FORCE_RES {
+    NONE = 0,
+    P720 = 4,
+    P480 = 3
+};
+
+extern int (OSDynLoad_Acquire)(const char* rpl, uint32_t *handle);
+
+extern int (OSDynLoad_FindExport)(uint32_t handle, int isdata, const char *symbol, void *address);
+
+void SwitchVideoMode() {
+		
+    unsigned int avm_handle;
+    OSDynLoad_Acquire("avm.rpl", & avm_handle);
+    int( * AVMSetTVScanResolution)(int r3);
+    int( * AVMGetCurrentPort)(int * port);
+
+    OSDynLoad_FindExport(avm_handle, 0, "AVMSetTVScanResolution", & AVMSetTVScanResolution);
+    OSDynLoad_FindExport(avm_handle, 0, "AVMGetCurrentPort", & AVMGetCurrentPort);
+
+    // out port shouldn't be needed, dont use if scart/composite or component
+    int outPort = 0;
+    AVMGetCurrentPort( & outPort);
+    if (outPort > 3) outPort = 0;
+    int wantRes = FORCERES;
+
+    if (outPort == 2 || outPort == 3) {
+        return;
+    } else {
+        AVMSetTVScanResolution(wantRes);
+    }
+
+}
 
 extern int CMPTLaunchTitle(void *CMPTConfigure, int ConfigSize, int titlehigh, int titlelow);
 
@@ -49,6 +87,10 @@ int main(int argc, char **argv) {
     }
 
     KPADInit();
+	
+	if (FORCERES != NONE) {
+	SwitchVideoMode();
+	}
 
     void *databuf = memalign(0x40, dataSize);
     CMPTLaunchTitle(databuf, dataSize, TIDHIGH, TIDLOW);
